826c7bea768ba5cf30b7f79a490ae7fae7afd3b1
Serap Altunbulak <56356727+SerapAltunbulak@users.noreply.github.com>
2021-10-04 17:11:17 +0300

    almost finished css

---
+++ script/main.js
@@ -0,0 +1,21 @@
+const bill = document.querySelector('.bill-charge');
+const custom = document.querySelector('#custom-tip');
+
+const five = document.querySelector('#tip-5');
+const ten = document.querySelector('#tip-10');
+const fifteen = document.querySelector('#tip-15');
+const twentyFive = document.querySelector('#tip-25');
+const fifty = document.querySelector('#tip-50');
+
+const tipPerPerson = document.querySelector('#total-tip-per-person');
+const totalPerPerson = document.querySelector('#total-amount-per-person');
+const resetButton = document.querySelector('#reset-button');
+
+const tipAmount = () => {
+    let tip = (bill.value % c) / p;
+}
+
+const total = () => {
+    let 
+}
+
